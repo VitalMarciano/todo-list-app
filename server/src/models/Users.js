a3d5288768fed9d8c0be7b76b70d1d2e9cd4e2d8
netaamz <netaamz@gmail.com>
2023-06-26 12:23:50 +0300

    minor fix

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true },
     password: {type: String, required: true },
-    username: {type: String, required: true },
-    password: {type: String, required: true },
+    email: {type: String, required: true },
     name: {type: String, required: true },
     lastName: {type: String, required: true },
     birthday: {type: String, required: true },
