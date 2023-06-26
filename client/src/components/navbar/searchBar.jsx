6a0cb963fb9b0a6b6b78bc62aa8307020d9a9e97
Vital Marciano <74133733+VitalMarciano@users.noreply.github.com>
2023-06-26 21:28:27 +0300

    fix z in search bar

--- client/src/components/navbar/searchBar.jsx
+++ client/src/components/navbar/searchBar.jsx
@@ -66,7 +66,7 @@ const SearchBar = () => {
           <div className="relative">
             <button
               id="dropdown-button"
-              className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+              className="flex-shrink-0 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
               type="button"
               onClick={toggleDropdown}
             >
