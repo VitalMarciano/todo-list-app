9f0775c4fdfb3744690e402e2737817423060911
Vital Marciano <74133733+VitalMarciano@users.noreply.github.com>
2023-07-17 13:35:47 +0300

    Update store.jsx

--- client/src/utils/store.jsx
+++ client/src/utils/store.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Dashboard from "../components/pages/dashboard";
 import Context from "./context";
-import Login from "../components/pages/Login";
+import Login from "../components/pages/login";
 
 const Store = () => {
   const { state, dispatch } = React.useContext(Context)
